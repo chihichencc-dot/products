9919e11f0733ebdf3feea00257f2c1a1b0096df0
CC <chihichen.cc@gmail.com>
2025-10-29 14:21:10 -0700

    add file check

--- products.py
+++ products.py
@@ -1,11 +1,23 @@
+import os
+
 products = []
-with open ('products.csv', 'r') as f:
-	for line in f:
-		if 'Item,Price' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('yes, file found!')
+	with open ('products.csv', 'r') as f:
+		for line in f:
+			if 'Item,Price' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+
+else:
+	print('nope!')
+#check if there is such file in the same path
+
+
+
+
 
 while True:
 	name = input('Please enter the products name:')
